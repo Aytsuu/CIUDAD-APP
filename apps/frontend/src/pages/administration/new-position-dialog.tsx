70037d79a9cd55cca27e3c827e8ef5a50bc4279f
aytsuu008 <paoloaraneta1237@gmail.com>
2025-01-30 13:20:35 +0800

    Modified

--- apps/frontend/src/pages/administration/new-position-dialog.tsx
+++ apps/frontend/src/pages/administration/new-position-dialog.tsx
@@ -15,34 +15,34 @@ import { Plus } from "lucide-react";
 
 export function NewPositionDialog() {
     return (
-      <Dialog>
-        <DialogTrigger asChild>
-            <Button 
-                variant='outline' 
-                className="border-2 border-blue text-darkBlue1 hover:bg-white hover:opacity-[80%]"
-            > 
-                <Plus className="text-darkBlue1"/> New Position
-            </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add</DialogTitle>
-            <DialogDescription>
-              Enter the name of the position to be added. Click save when you're done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <Dialog>
+          <DialogTrigger asChild>
+              <Button 
+                  variant='outline' 
+                  className="border-2 border-blue text-darkBlue1 hover:bg-white hover:opacity-[80%]"
+              > 
+                  <Plus className="text-darkBlue1"/> New Position
+              </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Add</DialogTitle>
+              <DialogDescription>
+                Enter the name of the position to be added. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+              </div>
             </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+            <DialogFooter>
+              <Button type="submit">Save</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
     )
 }
