a6c6b9940c6b74b1d03d228a0056e6729b4eb2f5
Sem <luizsemwarain@gmail.com>
2025-01-31 11:18:25 +0800

    modified form

---
+++ apps/frontend/src/form-schema/addevent-schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+
+// Shaping the complain form schema
+
+const AddEventFormSchema = z.object({
+    
+    eventTitle: z.string().min(1, 'Event title is required'),
+    eventDate: z.string().min(1, 'Event date address is required'),
+    roomPlace: z.string().min(1, 'Room / Place is required'),
+    eventCategory: z.string().min(1, 'Event category address is required'),
+    eventTime: z.string().min(1, 'Event time is required'),
+    eventDescription: z.string().min(1, 'Event Description is required'),
+    // barangayCouncil: z.string({ message: " " }),
+    // gadCommittee: z.string({ message: " " }),  
+    // wasteCommittee: z.string({ message: " " }),  
+
+    // complaintImg: z.instanceof(File)
+    // .refine(file => ['image/jpeg', 'image/jpg', 'image/png'].includes(file.type), {
+    //     message: 'Only JPEG/JPG, or PNG, files are allowed!',
+    // })
+    // .refine(file => file.size <= 5 * 1024 * 1024, {
+    //     message: 'File size must be less than 5MB!',
+    // })
+    // .nullable()
+
+});
+
+export default AddEventFormSchema;
--- apps/frontend/src/pages/AddEvent-Modal.tsx
+++ apps/frontend/src/pages/AddEvent-Modal.tsx
@@ -3,9 +3,17 @@ import {Input} from '../components/ui/input.tsx';
 import {Label} from '../components/ui/label.tsx';
 import {DatePicker} from '../components/ui/datepicker.tsx';
 import {Textarea} from '../components/ui/textarea.tsx';
-import {Select,SelectTrigger,SelectValue,SelectContent,SelectItem,SelectLabel,SelectSeparator,SelectGroup} from "../components/ui/select.tsx";
+import {Select,SelectTrigger,SelectValue,SelectContent,SelectItem,SelectLabel,SelectSeparator,SelectGroup} from "../components/ui/select/select.tsx";
 import {Button} from '../components/ui/button.tsx';
 import {FilterAccordion} from '../components/ui/filter-accordion.tsx'
+import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form";
+import { SelectLayout } from "@/components/ui/select/select-layout";
+
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import AddEventFormSchema from "@/form-schema/addevent-schema";
 
 function AddEvent(){
 
@@ -20,6 +28,7 @@ function AddEvent(){
     const checkboxclass = "inline-flex items-center gap-2";
 
 
+    //for the modal
     const [events, setEvents] = useState<{ start: Date; end: Date; title: string }[]>([]);
     const [showAttendees, setShowAttendees] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -63,87 +72,149 @@ function AddEvent(){
       setSelectedCategories([]);
     };
 
+    const form = useForm<z.infer<typeof AddEventFormSchema>>({
+        resolver: zodResolver(AddEventFormSchema),
+        defaultValues: {
+            eventTitle: "",
+            eventDate: "",
+            roomPlace: "",
+            eventCategory: "",
+            eventTime: "",
+            eventDescription: "",
+            // barangayCouncil: "",
+            // gadCommittee: "",
+            // wasteCommittee: "",
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof AddEventFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
 
     return(
         <div className="fixed inset-0 flex items-center justify-center bg-black/15 z-50">
-            <div className="p-8 w-[800px] mx-auto h-144 bg-white shadow-lg rounded-[10px]">
-                <form className="space-y-4">
-                    {/* First Modal Section */}
-                    {!showAttendees ? (
-                        <>
-                        <div className="flex justify-between items-center mb-6 pl-[571px]">
-                            <button
-                                className={textbuttoncss}
-                                onClick={() => setShowAttendees(true)} // Switch to the attendees section
-                                type="button"
-                                >
-                                NEXT &gt;
-                            </button>
-                        </div>
+            <div className="p-8 w-[900px] mx-auto h-[540px] bg-white shadow-lg rounded-[10px]">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
 
-                        <div className="mt-10">
-                            <Label className={inputlabelcss}>Meeting Title</Label>
-                            <Input id="meetTitle" placeholder="Enter Meeting Title" className={inputcss}></Input>
-                        </div>
+                        <h1 className="flex justify-center font-bold text-[30px] text-[#394360] pt-8">ADD EVENT</h1>
+                        <div className="overflow-y-auto max-h-[370px] scrollbar-custom pr-5">
+                            <div className="mt-10">
+                                <FormField
+                                    control={form.control}
+                                    name="eventTitle"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                        <FormLabel>Event Title</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter Event Title" className={inputcss} {...field}></Input>
+                                        </FormControl>
+                                        <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
 
-                        <div className="grid grid-cols-2 gap-4">
-                            <div>
-                                {/* <label className={inputlabelcss}>Date</label> */}
-                                <Label className={inputlabelcss}>Date</Label>
-                                {/* <input type="date" id="date" className={inputcss} /> */}
-                                <DatePicker></DatePicker>
+                            <div className="grid grid-cols-2 gap-4 mt-[20px]">
+                                <div>
+                                    <FormField
+                                        control={form.control}
+                                        name="eventDate"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                            <FormLabel>Event Date</FormLabel>
+                                            <FormControl>
+                                                <input type="date" {...field} className="mt-[8px] w-full border border-[#B3B7BD] p-1.5 shadow-sm sm:text-sm focus:outline-none rounded-[3px]" />
+                                            </FormControl>
+                                            <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div>
+                                    <FormField
+                                        control={form.control}
+                                        name="eventTime"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                            <FormLabel>Event Time</FormLabel>
+                                            <FormControl>
+                                                <input type="time" {...field} className="mt-[8px] w-full border border-[#B3B7BD] p-1.5 shadow-sm sm:text-sm focus:outline-none rounded-[3px]" />
+                                            </FormControl>
+                                            <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />                                    
+                                </div>
                             </div>
-                            <div>
-                                <Label className={inputlabelcss}>Room / Place</Label>
-                                <Input id="place" placeholder="Enter Room / Place" className={inputcss}></Input>
-                            </div>
-                        </div>
 
-                        <div className="grid grid-cols-2 gap-4">
-                            <div>
-                                <Label className={inputlabelcss}>Category</Label>
-                                    <Select>
-                                    <SelectTrigger>
-                                        <SelectValue placeholder="Select Category" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                    <SelectGroup>
-                                        <SelectLabel>Categories</SelectLabel>
-                                        <SelectSeparator />
-                                        <SelectItem value="Meeting">Meeting</SelectItem>
-                                        <SelectItem value="Activity">Activity</SelectItem>
-                                    </SelectGroup>
-                                    </SelectContent>
-                                </Select>
+                            <div className="grid grid-cols-2 gap-4 mt-[20px]">
+                                <div>
+                                    <FormField
+                                        control={form.control}
+                                        name="eventCategory"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                            <FormLabel>Event Category</FormLabel>
+                                            <FormControl>
+                                                <SelectLayout
+                                                    label="Categories"
+                                                    placeholder="Select Event Category"
+                                                    options={[
+                                                        {id: "1", name: "Meeting"},
+                                                        {id: "2", name: "Activity"}                                                   
+                                                    ]}
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div>
+                                    <FormField
+                                        control={form.control}
+                                        name="roomPlace"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                            <FormLabel>Room / Place</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Enter Room / Place" className={inputcss} {...field}></Input>
+                                            </FormControl>
+                                            <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
                             </div>
-                            <div>
-                                <label className={inputlabelcss}>Time</label>
-                                <input type="time" id="time" className={inputcss} />
-                            </div>
-                        </div>
 
-                        <div>
-                        <Label className={inputlabelcss}>Meeting Description</Label>
-                            <Textarea
-                                className="w-full border border-[#B3B7BD] p-2 shadow-sm focus:outline-none h-40 mt-[12px] rounded-[5px] resize-none"
-                                placeholder="Enter Meeting Description">
-                            </Textarea>
-                        </div>
-                        </>
-                    ) : (
-                        // Attendees Section
-                        <>
-                            <div className="flex justify-between items-center mb-6">
-                                <button
-                                className={textbuttoncss}
-                                onClick={() => setShowAttendees(false)} // Switch back to the first section
-                                type="button"
-                                >
-                                &lt; BACK
-                                </button>
+                            <div className="mt-[20px]">
+                                <FormField
+                                    control={form.control}
+                                    name="eventDescription"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                        <FormLabel>Room / Place</FormLabel>
+                                        <FormControl>
+                                            <Textarea
+                                                className="w-full border border-[#B3B7BD] p-2 shadow-sm focus:outline-none h-40 mt-[12px] rounded-[5px] resize-none"
+                                                placeholder="Enter Meeting Description"
+                                                {...field}>
+                                            </Textarea>
+                                        </FormControl>
+                                        <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />                                    
                             </div>
-                            <h1 className="flex justify-center font-bold text-[20px] text-[#394360] pb-8">ATTENDEES</h1>
-                            <div className="space-y-4 pb-20 overflow-y-auto max-h-[300px] scrollbar-custom pr-2">
+                            
+                            <h1 className="flex justify-center font-bold text-[20px] text-[#394360] pb-8 pt-8">ATTENDEES</h1>
+                            <div className="space-y-4 pb-20">
                                 {/* Attendees Checkboxes */}
                                 <FilterAccordion
                                     title="BARANGAY COUNCIL"
@@ -182,10 +253,10 @@ function AddEvent(){
                                 <Button type="submit" className="inline-block rounded-md border border-[#3D4C77] bg-[#3D4C77] px-8 py-2 text-sm font-medium text-white hover:bg-transparent hover:text-[#263D67] focus:outline-none focus:ring active:text-[#263D67]">
                                     Schedule                                                                                                                          
                                 </Button>
-                            </div>
-                        </>
-                    )}
-                </form>
+                            </div>                 
+                        </div>                                                                      
+                   </form>
+                </Form>                
             </div>
         </div>
     );
