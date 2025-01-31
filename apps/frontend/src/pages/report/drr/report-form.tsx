9e98c5e4de592f5ffd0e7d75fd144858784adf41
aytsuu008 <paoloaraneta1237@gmail.com>
2025-02-01 03:25:45 +0800

    Update

---
+++ apps/frontend/src/pages/report/drr/report-form.tsx
@@ -0,0 +1,16 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+import { 
+    Form,
+    FormControl,
+    FormField,
+    FormMessage,
+    FormItem,
+    FormLabel
+} from "@/components/ui/form";
+
+export default function ReportForm(){
+
+}
