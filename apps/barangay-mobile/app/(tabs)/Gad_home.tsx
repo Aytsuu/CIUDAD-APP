d3f0be49fa0c13ec97b9c84e201665fe2a4b74bb
Cracklings815 <lordplasm@gmail.com>
2025-02-01 23:09:30 +0800

    add features (request, report, health, calendar, pending request, donations)

--- apps/barangay-mobile/app/(tabs)/Gad_home.tsx
+++ apps/barangay-mobile/app/(tabs)/Gad_home.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
+import React from "react";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
 import "../../global.css";
-import { LinearGradient } from 'expo-linear-gradient';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import Entypo from '@expo/vector-icons/Entypo';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import BackgroundLogo_Ciudad from '@/assets/svg/BackgroundLogo_Ciudad';
-import BackgroundCiudad from '@/assets/svg/BackgroundCiudad';
-import DonationImage from '@/assets/svg/donationImage';
-import DonationImage2 from '@/assets/svg/donationImage2';
-import { useRouter } from 'expo-router';
-
+import { LinearGradient } from "expo-linear-gradient";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Entypo from "@expo/vector-icons/Entypo";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import BackgroundLogo_Ciudad from "@/assets/svg/BackgroundLogo_Ciudad";
+import BackgroundCiudad from "@/assets/svg/BackgroundCiudad";
+import DonationImage from "@/assets/svg/donationImage";
+import DonationImage2 from "@/assets/svg/donationImage2";
+import { useRouter } from "expo-router";
+import Features_Request_Icon from "@/assets/svg/Features_Request_Icon";
+import Features_Report_Icon from "@/assets/svg/Features_Report_Icon";
+import Features_Health_Icon from "@/assets/svg/Features_Health_Icon";
+import Features_Calendar_Icon from "@/assets/svg/Features_Calendar_Icon";
+import Features_PendingRequest_Icon from "@/assets/svg/Features_PendingRequest_Icon";
+import Features_Donations_Icon from "@/assets/svg/Features_Donations_Icon";
 
 export default function MyRequestScreen() {
-  
   const router = useRouter();
 
   const notif = () => {
-    router.push('/my-notification');
-  }; 
+    router.push("/my-notification");
+  };
 
   const donate = () => {
-    router.push('/donation-page');
-  }; 
+    router.push("/donation-page");
+  };
 
   return (
     //background
-    <ThemedView className="flex-1 relative">
-
+    <View className="flex-1 bg-[#ECF8FF] relative] ">
       <View className="absolute top-0 left-0 right-0 bottom-0">
         <BackgroundCiudad />
       </View>
@@ -37,10 +40,15 @@ export default function MyRequestScreen() {
         <BackgroundLogo_Ciudad />
       </View>
 
-      <ScrollView className="flex-1 px-4 pt-4" showsVerticalScrollIndicator={false}>
+      <ScrollView
+        className="flex-1 px-4 pt-4"
+        showsVerticalScrollIndicator={false}
+      >
         {/* header */}
         <View className="flex-row justify-between items-center mb-4">
-          <Text className="text-[25px] font-semibold text-white mt-9">My Home</Text>
+          <Text className="text-[25px] font-semibold text-white mt-9">
+            My Home
+          </Text>
           <View className="flex-row items-center mt-9">
             <TouchableOpacity className="mr-3">
               <Entypo name="dots-three-horizontal" size={24} color="white" />
@@ -65,29 +73,119 @@ export default function MyRequestScreen() {
 
         {/* donations */}
         <View className="mt-4 relative w-[357px] h-[100px] rounded-lg overflow-hidden">
-          <LinearGradient colors={['#00938B', '#002D2A']} start={{x:0, y:0}} end={{x:1, y:0}} className="absolute top-0 left-0 right-0 bottom-0 w-full h-full"/>
+          <LinearGradient
+            colors={["#00938B", "#002D2A"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            className="absolute top-0 left-0 right-0 bottom-0 w-full h-full"
+          />
 
-  
           <View className="p-4 z-10">
-            <Text className="text-white font-semibold text-[18px]">Give where the need is greatest.</Text>
-            <TouchableOpacity className="bg-[#00D707] w-[100px] h-[40px] px-5 py-3 rounded-full shadow-md self-center mt-2 ml-[-220px]" onPress={donate}>
-              <Text className="text-white font-bold text-base text-center">Donate</Text>
+            <Text className="text-white font-semibold text-[18px]">
+              Give where the need is greatest.
+            </Text>
+            <TouchableOpacity
+              className="bg-[#00D707] w-[100px] h-[40px] px-5 py-3 rounded-full shadow-md self-center mt-2 ml-[-220px]"
+              onPress={donate}
+            >
+              <Text className="text-white font-bold text-base text-center">
+                Donate
+              </Text>
             </TouchableOpacity>
           </View>
-
 
           <View className="absolute right-[-20px] top-0 bottom-12 w-[120px] h-[100px] ">
             <DonationImage2 />
           </View>
-  
+
           <View className="absolute right-[20px] top-12 bottom-0 w-[120px] h-[100px] ">
             <DonationImage />
           </View>
         </View>
 
         {/* features */}
-        <Text className="text-[18px] font-bold text-black mt-4">Features</Text>
+        <View className="px-4">
+          <Text className="text-[18px] font-bold text-black mt-4 ml-[-6]">
+            Features
+          </Text>
+
+          {/* first row */}
+          <View className="flex-row gap-4 mt-2 ml-[-6]">
+            {/* request */}
+            <TouchableOpacity className="w-48 h-20 flex-row items-center bg-white rounded-lg p-4 shadow-md">
+              <View className="w-12 h-12 bg-[#00B3FF] rounded-full flex items-center justify-center">
+                <Features_Request_Icon />
+              </View>
+              <Text className="ml-4 text-[18px] text-[#07143F] font-bold">
+                Request
+              </Text>
+            </TouchableOpacity>
+
+            {/* report */}
+            <TouchableOpacity className="w-48 h-20 flex-row items-center bg-white rounded-lg p-4 shadow-md">
+              <View className="w-12 h-12 bg-[#FF615F] rounded-full flex items-center justify-center">
+                <Features_Report_Icon />
+              </View>
+              <Text className="ml-4 text-[18px] text-[#07143F] font-bold">
+                Report
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* second row */}
+          <View className="flex-row gap-4 mt-2 ml-[-6]">
+            {/*health*/}
+            <TouchableOpacity className="w-48 h-20 flex-row items-center bg-white rounded-lg p-4 shadow-md">
+              <View className="w-12 h-12 bg-[#00CA5A] rounded-full flex items-center justify-center">
+                <Features_Health_Icon />
+              </View>
+              <Text className="ml-4 text-[18px] text-[#07143F] font-bold">
+                Health
+              </Text>
+            </TouchableOpacity>
+
+            {/*calendar*/}
+            <TouchableOpacity className="w-48 h-20 flex-row items-center bg-white rounded-lg p-4 shadow-md">
+              <View className="w-12 h-12 bg-[#FAB440] rounded-full flex items-center justify-center">
+                <Features_Calendar_Icon />
+              </View>
+              <Text className="ml-4 text-[18px] text-[#07143F] font-bold">
+                Calendar
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* 3rd row */}
+          <View className="flex-row gap-4 mt-2 ml-[-6]">
+            {/*health*/}
+            <TouchableOpacity className="w-48 h-20 flex-row items-center bg-white rounded-lg p-4 shadow-md">
+              <View className="w-12 h-12 bg-[#44A17C] rounded-full flex items-center justify-center">
+                <Features_PendingRequest_Icon />
+              </View>
+              <Text className="ml-4 text-[18px] text-[#07143F] font-bold">
+                Pending Request
+              </Text>
+            </TouchableOpacity>
+
+            {/*calendar*/}
+            <TouchableOpacity className="w-48 h-20 flex-row items-center bg-white rounded-lg p-4 shadow-md">
+              <View className="w-12 h-12 bg-[#FEF163] rounded-full flex items-center justify-center">
+                <Features_Donations_Icon />
+              </View>
+              <Text className="ml-4 text-[18px] text-[#07143F] font-bold">
+                Donations
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* i dont know unsay content ig scroll down */}
+        <View className="px-4 mt-6">
+          <Text className="text-[18px] font-bold text-black mt-4 ml-[-6]">
+            What's New For You
+          </Text>
+        </View>
       </ScrollView>
-    </ThemedView>
+    </View>
   );
 }
---
+++ apps/barangay-mobile/assets/svg/Features_Report_Icon.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Svg, { Path, Ellipse } from "react-native-svg";
+const Features_Report_Icon = (props) => (
+  <Svg
+    width={31}
+    height={39}
+    viewBox="0 0 31 39"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <Path
+      d="M7.16797 32.5H23.8346C24.9392 32.5 25.8346 31.6046 25.8346 30.5V8.5C25.8346 7.39543 24.9392 6.5 23.8346 6.5H7.16797C6.0634 6.5 5.16797 7.39543 5.16797 8.5V30.5C5.16797 31.6046 6.0634 32.5 7.16797 32.5Z"
+      fill="white"
+      stroke="#7A7A7A"
+      strokeLinecap="round"
+    />
+    <Path d="M12.918 22.75H9.04297" stroke="#7A7A7A" strokeLinecap="round" />
+    <Path d="M16.793 27.625H9.04297" stroke="#7A7A7A" strokeLinecap="round" />
+    <Path
+      d="M19.6589 12.1343C19.4898 11.2888 20.1365 10.5 20.9987 10.5V10.5C21.8609 10.5 22.5076 11.2888 22.3385 12.1343L21.4971 16.3414C21.4496 16.579 21.241 16.75 20.9987 16.75V16.75C20.7564 16.75 20.5478 16.579 20.5003 16.3414L19.6589 12.1343Z"
+      fill="#7A7A7A"
+      stroke="#7A7A7A"
+      strokeWidth={2}
+    />
+    <Ellipse
+      cx={21.0013}
+      cy={19.875}
+      rx={0.833333}
+      ry={1.25}
+      fill="#7A7A7A"
+      stroke="#7A7A7A"
+      strokeWidth={2}
+    />
+  </Svg>
+);
+export default Features_Report_Icon;
