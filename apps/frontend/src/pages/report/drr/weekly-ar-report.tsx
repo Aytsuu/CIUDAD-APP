dadf220a9da30c5e36ba8b31eb6850c3ba06b7c1
aytsuu008 <paoloaraneta1237@gmail.com>
2025-02-03 10:13:26 +0800

    update

--- apps/frontend/src/pages/report/drr/weekly-ar-report.tsx
+++ apps/frontend/src/pages/report/drr/weekly-ar-report.tsx
@@ -3,7 +3,7 @@ export default function WeeklyARReport(){
     return(
         <div className="w-full h-[100vh] flex justify-center items-center">
             <div className="w-1/2 h-2/3">
-
+                
             </div>
         </div>
     );
