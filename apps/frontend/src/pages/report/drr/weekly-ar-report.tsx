9a0df4e34d845be469fbffe209660da19be4f94d
aytsuu008 <paoloaraneta1237@gmail.com>
2025-02-03 11:16:14 +0800

    update

--- apps/frontend/src/pages/report/drr/weekly-ar-report.tsx
+++ apps/frontend/src/pages/report/drr/weekly-ar-report.tsx
@@ -1,9 +1,14 @@
 
 export default function WeeklyARReport(){
     return(
-        <div className="w-full h-[100vh] flex justify-center items-center">
-            <div className="w-1/2 h-2/3">
-                
+        <div className="w-full h-[100vh] bg-snow flex justify-center items-center">
+            <div className="w-[80%] h-2/3 flex gap-3">
+                <div className="w-full h-full bg-white border border-gray rounded-[5px]">
+
+                </div>
+                <div className="w-[30%] h-full bg-white border border-gray rounded-[5px]">
+
+                </div>
             </div>
         </div>
     );
