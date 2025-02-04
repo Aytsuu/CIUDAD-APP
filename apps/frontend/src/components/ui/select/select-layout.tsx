da4d01c0e27f160356d62ed7a517554767a6e5be
aytsuu008 <paoloaraneta1237@gmail.com>
2025-02-04 09:04:14 +0800

    Update Select-Layout

--- apps/frontend/src/components/ui/select/select-layout.tsx
+++ apps/frontend/src/components/ui/select/select-layout.tsx
@@ -10,7 +10,7 @@ import {
 
   interface Option{
     id: string,
-    name: string
+    name: React.ReactNode
   }
 
   interface SelectProps{
@@ -31,7 +31,7 @@ import {
           <SelectGroup>
             <SelectLabel>{label}</SelectLabel>
             {options.map((option)=>{
-                return <SelectItem key={option.id} value={option.name}>{option.name}</SelectItem>
+                return <SelectItem key={option.id} value={option.id}>{option.name}</SelectItem>
             })}
           </SelectGroup>
         </SelectContent>
