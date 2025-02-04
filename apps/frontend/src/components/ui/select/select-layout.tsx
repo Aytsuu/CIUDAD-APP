db3979601bcabc3971dbe7a383d2ed92942b85fb
aytsuu008 <paoloaraneta1237@gmail.com>
2025-02-04 09:03:47 +0800

    update

--- apps/frontend/src/components/ui/select/select-layout.tsx
+++ apps/frontend/src/components/ui/select/select-layout.tsx
@@ -7,10 +7,11 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select/select"
+import React from "react"
 
   interface Option{
     id: string,
-    name: string
+    name: React.ReactNode
   }
 
   interface SelectProps{
@@ -31,7 +32,7 @@ import {
           <SelectGroup>
             <SelectLabel>{label}</SelectLabel>
             {options.map((option)=>{
-                return <SelectItem key={option.id} value={option.name}>{option.name}</SelectItem>
+                return <SelectItem key={option.id} value={option.id}>{option.name}</SelectItem>
             })}
           </SelectGroup>
         </SelectContent>
