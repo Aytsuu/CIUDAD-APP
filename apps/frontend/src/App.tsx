3cf89431da2a072cb0616a8d3fda39e09852422c
aytsuu008 <paoloaraneta1237@gmail.com>
2025-02-06 18:27:01 +0800

    Update

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import MonthlyARReport from "./pages/report/drr/monthly-ar-report"
 import WeeklyARReport from "./pages/report/drr/weekly-ar-report"
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MonthlyARReport/>
+  }
+
+])
 
 function App() {
 
   return (
-    <>
-      <MonthlyARReport />
-    </> 
+    <RouterProvider router={router}/>
   )
 }
 
