91f959f4aef2f364891b28aecfcc3bf07ac54f97
Aytsuu <paoloaraneta008@gmail.com>
2025-02-17 03:45:32 +0800

    Update

--- apps/frontend/src/pages/administration/administrative-record.tsx
+++ apps/frontend/src/pages/administration/administrative-record.tsx
@@ -2,20 +2,124 @@ import { useState } from "react";
 
 import PaginationLayout from "@/components/ui/pagination/pagination-layout";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { ArrowUpDown, Plus } from "lucide-react";
 import { FilterAccordion } from "@/components/ui/filter-accordion";
-import { TableCaption } from "@/components/ui/table/table";
-import TableRecord from "./table-record";
-import { NewPositionDialog } from "./new-position-dialog";
+import { ColumnDef } from "@tanstack/react-table";
+import TooltipLayout from "@/components/ui/tooltip/tooltip-layout";
+import { DataTable } from "@/components/ui/table/data-table";
+import { Pen } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const categoryOptions = [
     { id: "1", label: "Profiling", checked: false },
     { id: "2", label: "Certification", checked: false },
     { id: "3", label: "Report", checked: false },
-  ];
+];
+
+type Record = {
+    id: string
+    lname: string
+    fname: string
+    mname: string
+    suffix: string
+    dateOfBirth: string
+    contact: number
+    dateAssigned: string
+    position: string
+}
+
+const records: Record[] = [
+    {
+        id: 'Lorem',
+        lname: 'Lorem',
+        fname: 'Lorem',
+        mname: 'Lorem',
+        suffix: 'Lorem',
+        dateOfBirth: 'Lorem',
+        contact: 1233,
+        dateAssigned: 'Lorem',
+        position: 'Lorem'
+    }
+]
+
+const columns: ColumnDef<Record>[] = [
+    {
+        accessorKey: 'lname',
+        header: ({column}) => (
+            <div
+                className="w-full h-full flex justify-center items-center gap-2 cursor-pointer"
+                onClick={() => (column.toggleSorting(column.getIsSorted() === "asc"))}
+            >
+                Last Name
+                <TooltipLayout
+                    trigger={<ArrowUpDown size={15}/>}
+                    content={"Sort"}
+                />
+            </div>
+        )
+    },
+    {
+        accessorKey: 'fname',
+        header: ({column}) => (
+            <div
+                className="w-full h-full flex justify-center items-center gap-2 cursor-pointer"
+                onClick={()=> (
+                    column.toggleSorting(column.getIsSorted() === "asc")
+                )}
+            >
+                First Name
+                <TooltipLayout
+                    trigger={<ArrowUpDown size={15} />}
+                    content={"Sort"}
+                />
+            </div>
+        )
+    },
+    {
+        accessorKey: 'mname',
+        header: ({column}) => (
+            <div
+                className="w-full h-full flex justify-center items-center gap-2 cursor-pointer"
+                onClick={()=> (
+                    column.toggleSorting(column.getIsSorted() === "asc")
+                )}
+            >
+                Middle Name
+                <TooltipLayout
+                    trigger={<ArrowUpDown size={15} />}
+                    content={"Sort"}
+                />
+            </div>
+        )
+    },
+    {
+        accessorKey: 'suffix',
+        header: 'Suffix'
+    },
+    {
+        accessorKey: 'dateOfBirth',
+        header: 'Date of Birth'
+    },
+    {
+        accessorKey: 'contact',
+        header: 'Contact'
+    },
+    {
+        accessorKey: 'dateAssigned',
+        header: 'Date Assigned'
+    },
+    {
+        accessorKey: 'action',
+        header: 'Action',
+        cell: ({row}) => (
+            <Button variant={"outline"}><Pen/></Button>
+        )
+    }
+]
 
 export function AdministrativeRecord(){
 
+    const data = records;
     const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
     const handleCategoryChange = (id: string, checked: boolean) => {
@@ -30,20 +134,21 @@ export function AdministrativeRecord(){
 
     return(
         <div className="relative w-full h-[100vh] bg-snow flex flex-col justify-center items-center">
-            <div className="w-[80%] h-2/3 flex flex-row gap-3">
-                <div className="w-full h-full bg-white border border-gray rounded-[10px] p-5 flex flex-col gap-3">
-                    <div className="w-full flex justify-end">
+            <div className="w-[80%] h-4/5 flex flex-col">
+                <div className="w-full h-full bg-white border border-gray rounded-[5px] flex flex-col">
+                    <div className="w-full flex justify-between p-5">
+                        <div className="w-1/4 flex">
+                            <Input type="text" placeholder="Search..."/>
+                        </div>
                         <Button 
-                            className="bg-blue hover:bg-blue hover:opacity-[80%]"
                         > 
                             <Plus /> Register a Staff
                         </Button>
                     </div>
-                    <TableRecord/>
-                    <PaginationLayout/>
+                    <DataTable columns={columns} data={data}/>
                 </div>
-                <div className="w-1/4 bg-white border border-gray rounded-[10px] p-5 flex flex-col gap-2">
-                    <NewPositionDialog/>
+                <PaginationLayout className="justify-end h-[10%]"/>
+                {/* <div className="w-1/4 bg-white border border-gray rounded-[10px] p-5 flex flex-col gap-2">
                     <FilterAccordion
                         title="Secretary"
                         options={categoryOptions.map((option) => ({
@@ -54,9 +159,8 @@ export function AdministrativeRecord(){
                         onChange={handleCategoryChange}
                         onReset={handleReset}
                     />
-                </div>
+                </div> */}
             </div>  
-            <TableCaption>A list of all staffs and positions.</TableCaption>
         </div>
     )
 
