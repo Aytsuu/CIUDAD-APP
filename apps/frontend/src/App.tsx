e18448c6cca11cce32aacacbf38501d0b4e0d330
Hannah Sheen <hannahsheen12@gmail.com>
2025-02-22 11:08:50 +0800

    filename updated (typo)

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,7 +4,7 @@ import ViewBudgetPlan from "./pages/treasurer-budgetplan/treasurer-budgetplan-vi
 import PersonalClearance from "./pages/treasurer-clearance-requests/treasurer-personalClearance"
 import PermitClearance from "./pages/treasurer-clearance-requests/treasurer-permitClearance"
 import ServiceCharge from "./pages/treasurer-clearance-requests/treasurer-serviceCharge"
-import BarangayService from "./pages/treasurer-clearance-requests/treasurer-barangarService"
+import BarangayService from "./pages/treasurer-clearance-requests/treasurer-barangayService"
 
 function App() {
 
