3b2d2121156e28ec8da7270cb96ff4e8d864eccd
Aytsuu <paoloaraneta008@gmail.com>
2025-02-24 02:12:37 +0800

    Updated root layout

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -6,6 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { NAV_THEME } from '@/lib/constants';
+import { PortalHost } from '@rn-primitives/portal';
 
 const LIGHT_THEME: Theme = {
   ...DefaultTheme,
@@ -34,12 +35,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={LIGHT_THEME}>
-      <Stack initialRouteName='(auth)'>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <StatusBar backgroundColor="#ECF8FF" style="dark" />
+      <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      <PortalHost />
     </ThemeProvider>
-    
   );
 }
