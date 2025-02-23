ba12a6bbb1601599e0f7f606d98741bcca45eec2
KattyShin <caballeskatrina@gmail.com>
2025-02-23 16:32:24 +0800

    all and inv record table created

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,18 @@
-function App() {
+// import ChildHealthForm from "./pages/CHR_INPUTFORM/childHR_Form"
+// import LastPage from "./pages/CHR_INPUTFORM/childHR_pagelast"
+import ChildHealthForm from "./pages/CHR_SERVICES/CHR_NEW_ADDFORM/childHR_Form"
+import AllChildHealthRecords from "./pages/CHR_SERVICES/CHR_RECORDS/childHR_all_records"
+import IndivChildHealthRecords from "./pages/CHR_SERVICES/CHR_RECORDS/childHR_inv_records"
+import InvMedicalConRecords from "./pages/MEDICAL_CONSULTATION_SERVICES/MEDICAL_RECORDS/medCon_InvRecords"
+import AllMedicalConRecords from "./pages/MEDICAL_CONSULTATION_SERVICES/MEDICAL_RECORDS/medCon_AllRecords"
 
+function App() {
   return (
-    <> 
-      
+    <>
+      <AllMedicalConRecords/>
     </> 
   )
 }
 
-export default App;
+export default App
+  
