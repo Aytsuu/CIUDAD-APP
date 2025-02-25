434a797beb6f5674f2cba8fdfd81d8e4a5b26cc9
KattyShin~ <caballeskatrina@gmail.com>
2025-02-25 10:24:19 +0800

    #patients queue created, modified DialogLayout

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,18 +1,3 @@
-// import PatientsQueueTable from "./pages/PATIENTSQUEUE/patientsQueueTable";
-// import PatientQueueForm from "./pages/PATIENTSQUEUE/patientQueueForm";
-// import MainPatientQueueTable from "./pages/PATIENTSQUEUE/mainPatientsQueue";
-// function App() {
-
-//   return (
-//     <> 
-//     <MainPatientQueueTable/>
-//     </> 
-//   )
-// }
-
-// export default App;
-
-
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router-dom";
 import { patientQueue } from "./routes/patientsQueue";
