d8831663b6baec4aab7a851011e11a20906ce931
Hannah Sheen <hannahsheen12@gmail.com>
2025-02-28 12:51:46 +0800

    budget main design updated

--- apps/frontend/src/pages/gad-budget-tracker/budget-tracker-main.tsx
+++ apps/frontend/src/pages/gad-budget-tracker/budget-tracker-main.tsx
@@ -8,29 +8,34 @@ function GADBudgetTrackerMain(){
 
     let year = "2020", remainingBal = 850000.00, budget = 1000000.00, income = 150000.00, expenses = 300000.00;
     const progress = budget > 0 ? (expenses / budget) * 100 : 0;
+    let styles = {
+        budgetLabel: "w-[12rem]",
+    }
 
     return(
-        <div className="bg-snow p-4 items-center">
-                <div className="mx-4 mb-4 mt-10 gap-4 flex flex-col p-5">
-                    <div className="flex flex-col gap-3 mb-4">
-                        <h1 className="font-semibold text-xl sm:text-2xl text-darkBlue2 flex flex-row items-center gap-2">
-                            <div>GAD Budget Tracker</div>
-                        </h1>
-                        <p className="text-xs sm:text-sm text-darkGray">
-                        Track and manage your income and expenses   with real-time insights.
-                        </p>
+        <div className="bg-snow w-full h-full">
+                    <div>
+                        <div className="mx-4 mb-4 mt-10 gap-4 flex flex-col p-5">
+                            <div className="flex flex-col gap-3 mb-4">
+                                <h1 className="font-semibold text-xl sm:text-2xl text-darkBlue2 flex flex-row items-center gap-2">
+                                    <div>GAD Budget Tracker</div>
+                                </h1>
+                                <p className="text-xs sm:text-sm text-darkGray">
+                                Track and manage your income and expenses with real-time insights.
+                                </p>
+                            </div>
+                            <hr className="border-gray mb-5 sm:mb-4" />   
+                        </div>
+
+                        <div className="flex justify-end gap-5 m-5">
+                            <Button> + Create New</Button>
+                            <Input className="w-[20rem] bg-white" placeholder="Search"></Input>
+                        </div>
                     </div>
-                    <hr className="border-gray mb-5 sm:mb-4" />   
-                </div>
-
-                <div className="flex justify-end gap-5 m-5">
-                    <Button> + Create New</Button>
-                    <Input className="w-[20rem] bg-white" placeholder="Search"></Input>
-                </div>
 
 
-                <div className="flex justify-center">
-                    <div className="border-2 border-solid border-gray-300 bg-white rounded-2xl m-5 p-8 w-full min-h-[250px]">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-5">
+                    <div className="border-2 border-solid border-gray-300 bg-white rounded-2xl p-8 w-full mb-4 min-h-[300px]">
                         <div className="flex flex-col gap-6">
                             <h1 className="font-semibold text-xl sm:text-2xl text-primary flex items-center gap-3">
                                 <div className="rounded-full border-2 border-solid border-primary p-3 flex items-center">
@@ -41,11 +46,11 @@ function GADBudgetTrackerMain(){
 
                             <div className="flex flex-col gap-4">
                                 <div className = "flex flex-row">
-                                    <Label className="w-[12rem]">Total Budget:</Label>
+                                    <Label className={styles.budgetLabel}>Total Budget:</Label>
                                     <Label className = "text-blue">Php {budget.toFixed(2)}</Label>
                                 </div>
                                 <div className = "flex flex-row">
-                                    <Label className="w-[12rem]">Total Income:</Label>
+                                    <Label className={styles.budgetLabel}>Total Income:</Label>
                                     <Label className = "text-green-600">Php {income.toFixed(2)}</Label>
                                 </div>
                                 <div className = "flex flex-row">
@@ -66,6 +71,7 @@ function GADBudgetTrackerMain(){
                             </div>
                         </div>
                     </div>
+                    
                 </div>
         </div>
     )
