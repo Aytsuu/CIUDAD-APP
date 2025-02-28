99c15f2a81c955fe5d67cfb75e442cc3b61a1689
Chris Tabs <157513312+chanixx786@users.noreply.github.com>
2025-02-28 19:15:29 +0800

    Update

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,4 @@
-import CreateAnnouncements from "./pages/announcement/CreateAnnouncement";
-
-
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import { landing_router } from './routes/landing-router';
 import { main_router } from './routes/main-router';
 
 const router = createBrowserRouter([
