90bf6cede385720200204892a7736c669389f85b
Chris Tabs <157513312+chanixx786@users.noreply.github.com>
2025-03-12 16:57:42 +0800

    backend config

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -3,5 +3,5 @@ import RegisterCompletion from "@/screens/auth/signup/RegisterCompletion";
 import Sample from "@/screens/sample";
 
 export default () => {
-    return <Sample />
+    return <LoginScreen />
 }
