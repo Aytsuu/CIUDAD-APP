1c465eda3b3b1f5cdd444395927c2c1a441fa80e
Aytsuu <paoloaraneta008@gmail.com>
2025-03-27 16:57:26 +0800

    Update

--- server-1/backend/urls.py
+++ server-1/backend/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
-    path('account/', include("apps.account.urls")),
     path('waste/', include('apps.waste.urls')),
     path('profiling/', include("apps.profiling.urls")),
     path('administration/', include("apps.administration.urls")),
