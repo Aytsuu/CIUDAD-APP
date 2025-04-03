b80fbeb91b952a9b7eb4e393c3a8bde4be64b027
Hannah Sheen <hannahsheen12@gmail.com>
2025-04-03 23:39:25 +0800

    added labelings on the table categories (budgetplan view)

--- web/src/pages/record/treasurer/treasurer-budgetplan/treasurer-budgetplan-view.tsx
+++ web/src/pages/record/treasurer/treasurer-budgetplan/treasurer-budgetplan-view.tsx
@@ -12,6 +12,8 @@ import { getBudgetDetails } from "./restful-API/budgetplanGetAPI";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const styles = {
+    mainCategory: "font-bold text-[19px] md:text-[22px]",
+    subCategory: "font-semibold text-[16px] md:text-[18px] text-sky-500",
     header: "font-bold text-lg text-blue-600",
     budgetItem: "flex flex-col space-y-1 p-3 rounded bg-white border-l-4 border-sky-500 shadow-sm",
     budgetLabel: "text-sm font-semibold text-gray-600",
@@ -36,6 +38,11 @@ interface BudgetPlan {
     plan_budgetaryObligations: number;
     plan_balUnappropriated: number;
     plan_issue_date: string;
+    plan_personalService_limit: number,
+    plan_miscExpense_limit: number,
+    plan_localDev_limit: number,
+    plan_skFund_limit: number,
+    plan_calamityFund_limit: number,
     budget_detail: BudgetPlanDetail[];
 }
 
@@ -55,7 +62,6 @@ function ViewBudgetPlan(){
     //get the passed Id
     const { plan_id } = useParams<{ plan_id: string }>();
     const [currentPage, setCurrentPage] = useState(1);
-
     const{ data: budgetDetails, isLoading: isLoadingBudgetDetails} = useQuery({
         queryKey: ['budgetDetails', plan_id],
         queryFn: () => getBudgetDetails(plan_id!),
@@ -63,40 +69,128 @@ function ViewBudgetPlan(){
         staleTime: 0
     });
 
+    // calculating net available resources
+    const availableResources =
+    (parseFloat(budgetDetails?.plan_balance) || 0) +
+    (parseFloat(budgetDetails?.plan_tax_share) || 0) +
+    (parseFloat(budgetDetails?.plan_tax_allotment) || 0) +
+    (parseFloat(budgetDetails?.plan_cert_fees) || 0) +
+    (parseFloat(budgetDetails?.plan_other_income) || 0);
+
+
+    // Limit Calculations
+    const personalService = budgetDetails?.plan_actual_income * (budgetDetails?.plan_personalService_limit / 100);
+    const miscExpense = budgetDetails?.plan_rpt_income * (budgetDetails?.plan_miscExpense_limit/ 100);
+    const localDev = budgetDetails?.plan_tax_allotment * (budgetDetails?.plan_localDev_limit/100);
+    const skfund = availableResources * (budgetDetails?.plan_skFund_limit/ 100);
+    const calamityfund = availableResources * (budgetDetails?.plan_calamityFund_limit/ 100);
+
+
     const rowsWithFooters = ["Membership Dues/Contribution to Organization", "Extraordinary & Miscellaneous Expense", "Subsidy to Sangguniang Kabataan (SK) FUnd"];
     const rowsWithLimitandBal = ["Commutation of Leave Credits", "Rehabilitation of Multi-Purpose", "Disaster Supplies"];
     const rowsWithTotal = ["Extraordinary & Miscellaneous Expense", "Total Capital Outlays"];
 
     // populating the rows
     const rowsProp = budgetDetails?.details?.reduce((acc: any[], detail: BudgetPlanDetail) => {
+
+        // adding title at the beginning of the table
+        if(acc.length == 0){
+            acc.push(
+                [<span className={styles.mainCategory}>CURRENT OPERATING EXPENDITURES</span>],
+                [<span className={styles.subCategory}>Personal Services</span>]
+            )
+        }
+
+        // populating the table
         const mainRow = [
             <span className={styles.rowItem}>{detail.dtl_budget_item}</span>,
             <span className={styles.rowValue}>{formatNumber(detail.dtl_proposed_budget)}</span>
         ];
 
-        if (detail.dtl_budget_item === "Membership Dues/Contribution to Organization" || detail.dtl_budget_item == "Extraordinary & Miscellaneous Expense"|| detail.dtl_budget_item == "Subsidy to Sangguniang Kabataan (SK) FUnd") {
+        if (detail.dtl_budget_item === "Membership Dues/Contribution to Organization") {
+            mainRow.push(
+                <span className={styles.rowValue}>{formatNumber(1000)}</span>,
+                <span className={styles.rowValue}>{formatNumber(1000)}</span>
+            );
+        }else if(detail.dtl_budget_item == "Extraordinary & Miscellaneous Expense"){
+            mainRow.push(
+                <span className={styles.rowValue}>{formatNumber(1000)}</span>,
+                <span className={styles.rowValue}>{formatNumber(1000)}</span>
+            );
+        }else if(detail.dtl_budget_item == "Subsidy to Sangguniang Kabataan (SK) Fund"){
             mainRow.push(
                 <span className={styles.rowValue}>{formatNumber(1000)}</span>,
                 <span className={styles.rowValue}>{formatNumber(1000)}</span>
             );
         }
-    
         acc.push(mainRow);
     
 
-        if (detail.dtl_budget_item === "Commutation of Leave Credits" || detail.dtl_budget_item == "Rehabilitation of Multi-Purpose" || detail.dtl_budget_item == "Disaster Supplies") {
+        if (detail.dtl_budget_item === "Commutation of Leave Credits") {
+            acc.push(
+                [
+                    <div ></div>, 
+                    <div className={styles.budgetFooter}>Total: {formatNumber(1000)}</div>,
+                    <div className={styles.budgetFooter}>{formatNumber(1000)}</div>,
+                    <div className={styles.budgetFooter}>{formatNumber(1000)}</div>
+                ],
+          
+                [<span className={styles.subCategory}>Maint. & Other Operating Expenses</span>]
+            );
+        } else if(detail.dtl_budget_item == "Disaster Supplies"){
+            acc.push(
+                [
+                    <div ></div>, 
+                    <div className={styles.budgetFooter}>Total: {formatNumber(1000)}</div>,
+                    <div className={styles.budgetFooter}>{formatNumber(1000)}</div>,
+                    <div className={styles.budgetFooter}>{formatNumber(1000)}</div>
+                ])
+        } else if(detail.dtl_budget_item == "Repair and Maintenance of Motor Vehicle"){
+
+            acc.push( 
+                // [],
+                [<span className={styles.subCategory}>Maint. & Other Operating Expenses</span>],
+                
+            )
+
+        }else if(detail.dtl_budget_item == "Insurance Expenses"){
+
             acc.push([
-                <div ></div>, 
-                <div className={styles.budgetFooter}>Total: {formatNumber(1000)}</div>,
-                <div className={styles.budgetFooter}>{formatNumber(1000)}</div>,
-                <div className={styles.budgetFooter}>{formatNumber(1000)}</div>
-            ]);
-        } else if(detail.dtl_budget_item == "Extraordinary & Miscellaneous Expense" || detail.dtl_budget_item == "Total Capital Outlays"){
+                <span className={styles.rowItem}>Other Maint. & Operating Expenses</span>
+            ])
+
+        }else if(detail.dtl_budget_item == "Rehabilitation of Multi-Purpose"){
+             acc.push(
+                [
+                    <div ></div>, 
+                    <div className={styles.budgetFooter}>Total: {formatNumber(1000)}</div>,
+                    <div className={styles.budgetFooter}>{formatNumber(1000)}</div>,
+                    <div className={styles.budgetFooter}>{formatNumber(1000)}</div>
+                ],
+                [],
+                [<span className={styles.subCategory}>Sangguniang Kabataan Fund</span>],
+        )
+        }else if(detail.dtl_budget_item == "Total Capital Outlays"){
             acc.push([
                 <div ></div>, 
                 <div className={styles.budgetFooter}>Total: {formatNumber(1000)}</div>,
             
-            ]);
+            ],
+            [<span className={styles.mainCategory}>NON-OFFICE</span>],
+            [<span className={styles.subCategory}>Local Development Fund</span>],
+        );
+
+        }else if(detail.dtl_budget_item == "Extraordinary & Miscellaneous Expense"){
+            acc.push([
+                <div ></div>, 
+                <div className={styles.budgetFooter}>Total: {formatNumber(1000)}</div>,
+            ],    
+            [<span className={styles.mainCategory}>CAPITAL OUTLAYS</span>],
+            );
+        } else if(detail.dtl_budget_item == "Subsidy to Sangguniang Kabataan (SK) Fund"){
+            acc.push(
+                [<span className={styles.subCategory}>LDRRM Fund (Calamity Fund)</span>],
+            )
         }
     
         return acc;
@@ -113,14 +207,6 @@ function ViewBudgetPlan(){
             </div>
         )
     }
-
-    // calculating net available resources
-    const availableResources =
-    (parseFloat(budgetDetails?.plan_balance) || 0) +
-    (parseFloat(budgetDetails?.plan_tax_share) || 0) +
-    (parseFloat(budgetDetails?.plan_tax_allotment) || 0) +
-    (parseFloat(budgetDetails?.plan_cert_fees) || 0) +
-    (parseFloat(budgetDetails?.plan_other_income) || 0);
 
     // Pagination
     const totalPages = Math.ceil(rowsProp.length / 12);
