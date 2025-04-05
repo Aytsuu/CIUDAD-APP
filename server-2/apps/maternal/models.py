10ed24e85bbea7573c9bd88b1e0f95903bb70c9d
m0chiiiiii <marielamit03@gmail.com>
2025-04-05 08:13:58 +0800

    r u eyten 2 twenty yrs old

--- server-2/apps/maternal/models.py
+++ server-2/apps/maternal/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 from django.core.validators import MinValueValidator
 
 # Create your models here.
-class PrenatalForm(models.Model):
+class Prenatal_Form(models.Model):
     pf_id = models.BigAutoField(primary_key=True)
     pf_h_lname = models.CharField(max_length=100)
     pf_h_fname = models.CharField(max_length=100)
@@ -20,14 +20,73 @@ class PrenatalForm(models.Model):
 
 class Obstretical_History(models.Model):
     obs_id = models.BigAutoField(primary_key=True)
-    obs_ch_born_alive = models.PositiveBigIntegerField()
-    obs_living_ch = models.PositiveBigIntegerField()
-    obs_abortion = models.PositiveBigIntegerField()
-    obs_still_birth = models.PositiveBigIntegerField()
-    obs_lg_babies = models.PositiveBigIntegerField()
-    obs_gravida = models.PositiveBigIntegerField()
-    obs_para = models.PositiveBigIntegerField()
-    obs_fullterm = models.PositiveBigIntegerField()
-    obs_preterm = models.PositiveBigIntegerField()
+    obs_ch_born_alive = models.PositiveIntegerField()
+    obs_living_ch = models.PositiveIntegerField()
+    obs_abortion = models.PositiveIntegerField()
+    obs_still_birth = models.PositiveIntegerField()
+    obs_lg_babies = models.PositiveIntegerField()
+    obs_gravida = models.PositiveIntegerField()
+    obs_para = models.PositiveIntegerField()
+    obs_fullterm = models.PositiveIntegerField()
+    obs_preterm = models.PositiveIntegerField()
     obs_record_from = models.CharField(max_length=100)
+    # health profiling fk
+
+    class Meta:
+        db_table = 'obstretical_history'
+
+# next: illness
+
+class Previous_Hospitalization(models.Model):
+    pfph_id = models.BigAutoField(primary_key=True)
+    pfph_prev_hospi = models.CharField(max_length=250)
+    pfph_prev_hospi_year = models.PositiveIntegerField()
+    pf_id =models.ForeignKey(Prenatal_Form, on_delete=models.CASCADE,)
+
+    class Meta:
+        db_table = 'pf_previous_hospitalization'
+
+class Previous_Pregnancy(models.Model):
+    pfpp_id = models.BigAutoField(primary_key=True)
+    pfpp_date_of_delivery = models.DateField()
+    pfpp_outcome = models.CharField(max_length=50)
+    pfpp_type_of_delivery = models.CharField(max_length=50)
+    pfpp_babys_wt = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
+    pfpp_gender = models.CharField(max_length=10)
+    pfpp_ballard_score = models.PositiveIntegerField()
+    pfpp_apgar_score = models.PositiveIntegerField()
+    pf_id = models.ForeignKey(Prenatal_Form, on_delete=models.CASCADE,)
     
+    class Meta:
+        db_table = 'pf_previous_pregnancy'
+
+class TT_Status(models.Model):
+    pfts_id = models.BigAutoField(primary_key=True)
+    pfts_vaccine_type = models.CharField(max_length=100)
+    pfts_status = models.CharField(max_length=10)
+    pfts_date_given = models.DateField()
+    pfts_tdap = models.BooleanField(default=False)
+    pf_id = models.ForeignKey(Prenatal_Form, on_delete=models.CASCADE,)
+
+    class Meta:
+        db_table = 'pf_tt_status'
+
+class Lab_Result(models.Model):
+    pflr_id = models.BigAutoField(primary_key=True)
+    pflr_urinalysis = models.DateField()
+    pflr_cbc = models.DateField()
+    pflr_sgot_sgpt = models.DateField()
+    pflr_creatinine_serum = models.DateField()
+    pflr_bua_bun = models.DateField()
+    pflr_syphillis = models.DateField()
+    pflr_hiv_test = models.DateField()
+    pflr_hepa_b = models.DateField()
+    pflr_ogct_50 = models.DateField()
+    pflr_ogct_100 = models.DateField()
+    pflr_lab_remarks = models.CharField(max_length=250)
+    pf_id = models.ForeignKey(Prenatal_Form, on_delete=models.CASCADE,)
+
+    class Meta:
+        db_table = 'pf_lab_result'
+
+# next: lab result docs
--- server-2/apps/maternal/serializer.py
+++ server-2/apps/maternal/serializer.py
@@ -2,12 +2,34 @@ from rest_framework import serializers
 from .models import *
 from datetime import date
 
-class Prenatal_Form(serializers.ModelSerializer):
+class Prenatal_Form_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Prenatal_Form
         fields = '__all__'
 
-class Obstretical_History(serializers.ModelSerializer):
+class Obstretical_History_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Obstretical_History
+        fields = '__all__'
+
+# illness serializer
+
+class Previous_Hospitalization_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Previous_Hospitalization
+        fields = '__all__'
+
+class Previous_Pregnancy_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Previous_Pregnancy
+        fields = '__all__'
+
+class TT_Status_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = TT_Status
+        fields = '__all__'
+
+class Lab_Result_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lab_Result
         fields = '__all__'
