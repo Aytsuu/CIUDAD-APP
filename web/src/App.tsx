63aa67727c6ee64b68382fed7e80668c65e4ed3b
lenria <jarlenemaria.garcia@gmail.com>
2025-04-10 14:17:14 +0800

    basta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import { main_router } from './router/main-router';
-import { landing_router } from './router/landing-router';
-import { settings_router } from './router/settings';
+import { main_router } from './routers/main-router';
+import { landing_router } from './routers/landing-router';
+import { settings_router } from './routers/settings';
 
 const router = createBrowserRouter([
   ...main_router,
