0f49f6ac72a2563f1293b20b75c6786a73296f9c
KattyShin~ <caballeskatrina@gmail.com>
2025-04-16 22:30:33 +0800

    updated

--- web/src/components/ui/form/form-select.tsx
+++ web/src/components/ui/form/form-select.tsx
@@ -9,7 +9,6 @@ import {
 import { SelectLayout } from "../select/select-layout";
 import { Input } from "@/components/ui/input";
 
-<<<<<<< HEAD
 // Reusable Form Select Component with integrated loading/empty states
 export const FormSelect = React.memo(({ 
     control, 
@@ -73,33 +72,3 @@ export const FormSelect = React.memo(({
         />
     );
 });
-=======
-// Reusable Form Select Component
-export const FormSelect = React.memo(({ control, name, label, options, readOnly }: 
-  { control: any; name: string; label?: string; options: { id: string; name: string }[]; readOnly?: boolean }
-) => (
-  <FormField
-    control={control}
-    name={name}
-    render={({ field }) => (
-        <FormItem>
-          <FormLabel className="text-black/70">{label}</FormLabel>
-          <FormControl>
-            {!readOnly ? (
-              <SelectLayout
-                placeholder="Select"
-                className="w-full"
-                options={options}
-                value={field.value?.toLowerCase() ?? ""}
-                onChange={field.onChange}
-              />
-            ) : (
-              <Input {...field} readOnly />
-            )}
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-  />
-));
->>>>>>> a89fc25e6a5ac2c76e6cd07efb80cabc48b09f0a
