2118224c6a8546f81809234e5fa40953638b3850
m0chiiiiii <marielamit03@gmail.com>
2025-04-21 18:34:38 +0800

    tested prenatal_form and obstetrical_history and ok-ka

--- server-2/apps/maternal/urls.py
+++ server-2/apps/maternal/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns=[
     path("prenatal_record/", PrenatalFormView.as_view(), name="prenatalrecord"),
-    path("obstetrical_history/", ObstetricalHistoryView.as_view, name="obstetricalhistory"),
+    path("obstetrical_history/", ObstetricalHistoryView.as_view(), name="obstetricalhistory"),
     path("previous_hospitalization/", PreviousHospitalizationView.as_view(), name="previoushospitalization"),
     path("previous_pregnancy/", PreviousPregnancyView.as_view(), name="previouspregnancy"),
     path("tt_status/", TTStatusView.as_view(), name="ttstatus"),
