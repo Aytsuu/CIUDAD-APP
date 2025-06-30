d6fb4ac1cd5c5a924f6e1e1298e554b30c6f2938
Joshua Ganzo <ganzoganzo188@gmail.com>
2025-07-01 07:33:18 +0800

    update

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,6 +1,7 @@
 import LoginScreen from "@/screens/auth/login";
 import UploadID from "@/screens/auth/signup/UploadID";
+import HomeScreen from "../(tabs)";
 
 export default () => {
-    return <LoginScreen/>
+    return <HomeScreen/>
 }
