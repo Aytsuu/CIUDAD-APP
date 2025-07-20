9631aff6754010f262cfe38b44eec80b18b06f73
Aytsuu <paoloaraneta008@gmail.com>
2025-07-20 22:43:49 +0800

    Configure render.yml, build.sh, and update settings.py for production

--- server-1/backend/asgi.py
+++ server-1/backend/asgi.py
@@ -13,18 +13,9 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from apps.notification.routing import websocket_urlpatterns
+# from apps.notification.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
    
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        )
-    )
-})
+application = get_asgi_application()
