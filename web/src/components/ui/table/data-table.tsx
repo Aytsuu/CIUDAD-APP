0159e5b740c1b41c9aab4ba29ef0cb69ff838510
Sem <luizsemwarain@gmail.com>
2025-08-03 09:42:14 +0800

    fixed data-table compo

--- web/src/components/ui/table/data-table.tsx
+++ web/src/components/ui/table/data-table.tsx
@@ -7,7 +7,6 @@ import {
     SortingState,
     VisibilityState,
     getFilteredRowModel,
-    getPaginationRowModel,
     getSortedRowModel,
     useReactTable,
   } from "@tanstack/react-table"
@@ -20,53 +19,85 @@ import {
     TableHeader,
     TableRow,
   } from "./table"
+import { Loader2 } from "lucide-react"
+import { cn } from "@/lib/utils"
    
   interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
-    header?: boolean
-    onRowClick?: (row: TData) => void
+    header?: boolean;
+    headerClassName?: string;
+    cellClassName?: string;
+    isLoading?: boolean;
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+    reset?: boolean;
+    setReset?: React.Dispatch<React.SetStateAction<boolean>>
+    onSelectedRowsChange?: (rows: TData[]) => void;
+    rowSelection?: Record<string, boolean>;
+    setRowSelection?: React.Dispatch<React.SetStateAction<Record<string, boolean>>>
   }
    
-  export function DataTable<TData, TValue>({ 
+  export function DataTable<TData, TValue>({
+    isLoading=false, 
+    headerClassName, 
+    cellClassName,
+    header=true, 
     columns, 
-    data, 
-    onRowClick, 
-    header = false 
+    data,
+    reset,
+    setReset,
+    onSelectedRowsChange,
+    rowSelection: externalRowSelection,
+    setRowSelection: externalSetRowSelection
   }: DataTableProps<TData, TValue>) {
 
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
     const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
-    const [rowSelection, setRowSelection] = React.useState({})
+    const [internalRowSelection, setInternalRowSelection] = React.useState({});
+    const rowSelectionState = externalRowSelection ?? internalRowSelection;
+    const setRowSelectionState = externalSetRowSelection ?? setInternalRowSelection;
 
     const table = useReactTable({
-        data,
-        columns,
-        onSortingChange: setSorting,
-        onColumnFiltersChange: setColumnFilters,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getFilteredRowModel: getFilteredRowModel(),
-        onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
-        state: {
-            sorting,
-            columnFilters,
-            columnVisibility,
-            rowSelection,
-        },
+      data,
+      columns,
+      onSortingChange: setSorting,
+      onColumnFiltersChange: setColumnFilters,
+      getCoreRowModel: getCoreRowModel(),
+      getSortedRowModel: getSortedRowModel(),
+      getFilteredRowModel: getFilteredRowModel(),
+      onColumnVisibilityChange: setColumnVisibility,
+      onRowSelectionChange: setRowSelectionState,
+      enableRowSelection: true,
+      state: {
+        sorting,
+        columnFilters,
+        columnVisibility,
+        rowSelection: rowSelectionState,
+      },
     })
+
+     React.useEffect(() => {
+      if (onSelectedRowsChange) {
+        const selectedRows = table.getSelectedRowModel().rows.map(row => row.original)
+        onSelectedRowsChange(selectedRows)
+      }
+    }, [onSelectedRowsChange, rowSelectionState]);
+
+    React.useEffect(() => {
+      if(reset) {
+        table.resetRowSelection();
+        setReset && setReset(false);
+      }
+    }, [reset])
    
     return (
         <Table>
           {header && (<TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id} className="border-none bg-lightBlue hover:bg-lightBlue h-12">
+              <TableRow key={headerGroup.id} className="bg-lightBlue hover:bg-lightBlue h-10">
                 {headerGroup.headers.map((header) => {
                   return (
-                    <TableHead key={header.id} className="text-center">
+                    <TableHead key={header.id} className={cn("text-center", headerClassName)}>
                       {header.isPlaceholder
                         ? null
                         : flexRender(
@@ -80,17 +111,16 @@ import {
             ))}
           </TableHeader>)
           }
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
+          <TableBody className="overflow-auto">
+            {!isLoading ? (table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
                   data-state={row.getIsSelected() && "selected"}
-                  onClick={() => onRowClick?.(row.original)}
-                  className={onRowClick ? "cursor-pointer" : ""}
+                  className={!header ? "border-none hover:bg-white" : ""}
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id} className="text-center">
+                    <TableCell key={cell.id} className={cn("text-center", cellClassName)}>
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
                   ))}
@@ -100,6 +130,14 @@ import {
               <TableRow>
                 <TableCell colSpan={columns.length} className="h-24 text-center">
                   No results.
+                </TableCell>
+              </TableRow>
+            )) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24">
+                  <div className="w-full h-full flex items-center justify-center">
+                    <Loader2 className="animate-spin opacity-50"/>
+                  </div>
                 </TableCell>
               </TableRow>
             )}
