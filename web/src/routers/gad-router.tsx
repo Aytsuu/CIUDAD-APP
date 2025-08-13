0c76f3f5b73ab286ba8f62fd48b8bb0b78b578a9
Hannah Sheen <hannahsheen12@gmail.com>
2025-08-13 14:49:56 +0800

    bug fix for deployment

--- web/src/routers/gad-router.tsx
+++ web/src/routers/gad-router.tsx
@@ -1,6 +1,6 @@
 import BudgetTracker from "@/pages/record/gad/budget-tracker/budget-tracker-table";
 import GADBudgetTrackerMain from "@/pages/record/gad/budget-tracker/budget-tracker-main";
-import GADEditEntryForm from "@/pages/record/gad/budget-tracker/budget-tracker-edit-form";
+// import GADEditEntryForm from "@/pages/record/gad/budget-tracker/budget-tracker-edit-form";
 import GADProjectProposal from "@/pages/record/gad/project-proposal/project-proposal-main";
 import AnnualDevelopmentPlan from "@/pages/record/gad/annual_development_plan/annual_development_plan_main";
 import AnnaualDevelopmentPlanTable from "@/pages/record/gad/annual_development_plan/annual_development_table";
@@ -14,10 +14,10 @@ export const gad_router = [
         path: '/gad/gad-budget-tracker-table/:year/',
         element: <BudgetTracker/>
     },
-    {
-        path: 'gad-budget-tracker-entry/:gbud_num/',
-        element: <GADEditEntryForm />,
-    },
+    // {
+    //     path: 'gad-budget-tracker-entry/:gbud_num/',
+    //     element: <GADEditEntryForm />,
+    // },
     {
         path: '/gad-budget-tracker-main',
         element: <GADBudgetTrackerMain/>
