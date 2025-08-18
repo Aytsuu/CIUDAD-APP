092fd0143a289f1bc192a36ac71ee9397c9470a3
Hannah Sheen <hannahsheen12@gmail.com>
2025-08-18 22:31:30 +0800

    complaint manual merge

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "http://172.16.80.201:8000",
+    baseURL: "http://10.192.82.53:8000",
     headers: {
         'Content-Type': 'application/json', // Ensure JSON is used for requests
     }, 
