9c6f889630b4b2b392e1926a7a93245cef8e5818
Baneney <luizsemwarain@gmail.com>
2025-08-28 21:49:56 +0800

    goods cr_date_completed

--- web/src/pages/record/clearances/restful-api/certificatePutAPI.tsx
+++ web/src/pages/record/clearances/restful-api/certificatePutAPI.tsx
@@ -9,6 +9,7 @@ export const updateCertificationStatus = async (cr_id: string) => {
 
         const res = await api.put(`clerk/certificate-update-status/${cr_id}/`,{
             cr_req_status: "Completed",
+            cr_date_completed: new Date().toISOString()
         })
 
         return res.data;
