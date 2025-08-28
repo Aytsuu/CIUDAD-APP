c8a5eeb8a2f7004440af06e9e739617cdceaf0d4
Baneney <luizsemwarain@gmail.com>
2025-08-29 04:28:42 +0800

    adjusting the certification file

--- web/src/pages/record/clearances/Certification.tsx
+++ web/src/pages/record/clearances/Certification.tsx
@@ -539,8 +539,7 @@ import { toast } from "sonner";
 import TemplateMainPage from "../council/templates/template-main";
 import { calculateAge } from '@/helpers/ageCalculator';
 import { useUpdateCertStatus, useUpdateNonCertStatus } from "./queries/certUpdateQueries";
-import DialogLayout from "@/components/ui/dialog/dialog-layout";
-import { formatTimestamp } from "@/helpers/timestampformatter";
+import { localDateFormatter } from "@/helpers/localDateFormatter";
 
 function CertificatePage() {
   const navigate = useNavigate();
@@ -601,7 +600,7 @@ function CertificatePage() {
         else{
           await updateNonResStatus(Number(variables.nrc_id))
         }
-        
+
         setSelectedCertificate(null);
       } catch (error) {
         toast.error("First mutation succeeded but second failed");
@@ -691,7 +690,7 @@ function CertificatePage() {
     {
       accessorKey: "req_request_date",
       header: "Date Requested",
-      cell: ({ row }) => <div>{formatTimestamp(row.getValue("req_request_date"))}</div>,
+      cell: ({ row }) => <div>{localDateFormatter(row.getValue("req_request_date"))}</div>,
     },
     {
       accessorKey: "req_purpose",
