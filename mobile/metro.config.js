4d0f5db5286e87c6c128d951bb2f955da98bc627
Cracklings815 <lordplasm@gmail.com>
2025-08-28 12:39:37 +0800

    mobile certificate request

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+// Use expo-fetch to resolve whatwg-fetch
+config.resolver.alias = {
+    'whatwg-fetch': 'expo-fetch',
+};
+
+module.exports = withNativeWind(config, { input: './global.css' });
