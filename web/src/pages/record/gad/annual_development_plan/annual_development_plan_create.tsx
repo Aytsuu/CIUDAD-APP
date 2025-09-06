7ebe4bc2f3d6e17805f8936c8b221970803f36ee
Cracklings815 <lordplasm@gmail.com>
2025-09-06 11:56:54 +0800

    added auth for the cert mobile

--- web/src/pages/record/gad/annual_development_plan/annual_development_plan_create.tsx
+++ web/src/pages/record/gad/annual_development_plan/annual_development_plan_create.tsx
@@ -516,7 +516,7 @@ export default function AnnualDevelopmentPlanCreate() {
                               </button>
                             </div>
                           </div>
-                          <p className="text-sm text-gray-600">Quantity: {item.gdb_pax} Price: ₱{price.toFixed(2)}</p>
+                          <p className="text-sm text-gray-600">Quantity: {item.gdb_pax} | Price: ₱{price.toFixed(2)}</p>
                         </div>
                       );
                     })}
