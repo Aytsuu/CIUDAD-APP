693031e7a146a85857f84c7199dc45b818a5f062
Aytsuu <paoloaraneta008@gmail.com>
2025-09-09 01:12:48 +0800

    Update

--- web/src/pages/record/profiling/resident/RegistrationLayout.tsx
+++ web/src/pages/record/profiling/resident/RegistrationLayout.tsx
@@ -198,6 +198,7 @@ export default function RegistrationLayout() {
 
     const {per_id, ...personal} = personalSchema
     const {files, ...business} = businessSchema
+    const {confirm_password, ...account} = accountSchema
     
     const newFiles = files?.map((media: any) => ({
       name: media.name,
@@ -214,7 +215,7 @@ export default function RegistrationLayout() {
           noFamily,
           noBusiness,
           capitalizeAllFields(personal),
-          accountSchema,
+          account,
           houseSchema,
           livingSoloSchema,
           familySchema,
