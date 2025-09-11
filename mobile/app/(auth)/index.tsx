d29c0c403c205c68bc5b686021a658c6d0728903
Baneney <luizsemwarain@gmail.com>
2025-09-11 12:21:16 +0800

    cleaning waste collection (mobile)

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -4,6 +4,7 @@ import ResolutionPage from "@/screens/council/resolution/resolution-main";
 import WasteIllegalDumpingResMain from "@/screens/report/illegal-dumping/resident/illegal-dump-main-res";
 import WasteIllegalDumping from "@/screens/report/illegal-dumping/staff/illegal-dump-main-staff";
 import IllegalDumpCreateForm from "@/screens/report/illegal-dumping/resident/illegal-dump-create";
+import WasteCollectionMain from "@/screens/waste/waste-collection/waste-collection-main";
 
 // Makes the Error Overlay dissapear
 import { LogBox } from 'react-native';
@@ -13,6 +14,6 @@ if (__DEV__) {
 }
 
 export default () => {
-    return <WasteIllegalDumping/>
+    return <WasteCollectionMain/>
 }
  
