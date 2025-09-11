5d5cbfdf905200e54060bb0ee23be446a32cd74e
Baneney <luizsemwarain@gmail.com>
2025-09-11 14:10:24 +0800

    update illegal-dump (web)

--- web/src/pages/record/report/waste/queries/waste-ReportUpdateQueries.tsx
+++ web/src/pages/record/report/waste/queries/waste-ReportUpdateQueries.tsx
@@ -24,6 +24,7 @@ export const useUpdateWasteReport = (rep_id: number, onSuccess?: () => void) => 
       // 1. Update the main report status and date
       await updateWasteReport(rep_id, {
         rep_status: values.rep_status,
+        staff_id: values.staff_id
       });
       
       console.log("GAWAS SA QUERY: ", values.files)
