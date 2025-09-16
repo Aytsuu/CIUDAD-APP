5734a549d5513fc7567ee4b83b875824d8f8fdba
KattyShin~ <caballeskatrina@gmail.com>
2025-09-16 17:31:30 +0800

    api modified

--- web/src/api/api.tsx
+++ web/src/api/api.tsx
@@ -1,26 +1,8 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   withCredentials: true,
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json",
-//   },
-// });
-
-// export const api2 = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL2,
-//   withCredentials: true,
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json",
-//   },
-// });
-
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
-  withCredentials: true, 
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
@@ -28,8 +10,26 @@ export const api = axios.create({
 });
 
 export const api2 = axios.create({
-  baseURL: "http://localhost:8001",
+  baseURL: import.meta.env.VITE_API_URL2,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
 });
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:8000",
+//   withCredentials: true, 
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+// });
+
+// export const api2 = axios.create({
+//   baseURL: "http://localhost:8001",
+// });
 
 
 
