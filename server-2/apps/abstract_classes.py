0b408867c4e36a175dc23578a1182a22b2f8afd1
betaTrident <colinakb24@gmail.com>
2025-09-17 21:07:28 +0800

    update

---
+++ server-2/apps/abstract_classes.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class AbstractModels(models.Model):
+  class Meta:
+    abstract = True
+  
+  def save(self, *args, **kwargs):
+    for field in self._meta.fields:
+      if(
+        isinstance(field, (models.CharField, models.TextField)) 
+        and not field.primary_key
+        and field.editable
+      ) :
+        val = getattr(self, field.name)
+        if isinstance(val, str):
+          setattr(self, field.name, val.upper())
+    
+    super().save(*args, **kwargs)
