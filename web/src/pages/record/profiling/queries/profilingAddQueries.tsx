1eb2a85e01a650db85a8c9a37d7f25bcf2540eb4
Aytsuu <paoloaraneta008@gmail.com>
2025-09-17 14:47:09 +0800

    Update

--- web/src/pages/record/profiling/queries/profilingAddQueries.tsx
+++ web/src/pages/record/profiling/queries/profilingAddQueries.tsx
@@ -10,7 +10,7 @@ import {
   addPersonalAddress,
   addResidentAndPersonal,
 } from "../restful-api/profiingPostAPI";
-import { api, api2 } from "@/api/api";
+import { api } from "@/api/api";
 
 export const useAddSitio = () => {
   const queryClient = useQueryClient();
