0847d5c8c5036e9fa4fc4dff9a103c3336bba2c3
Baneney <luizsemwarain@gmail.com>
2025-09-21 12:32:10 +0800

    update in inc-exp (mobile)

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -4,6 +4,7 @@ import ReceiptPage from "@/screens/treasurer/receipt/receipt-main";
 import ResolutionPage from "@/screens/council/resolution/resolution-main";
 import WasteCollectionMain from "@/screens/waste/waste-collection/waste-collection-main";
 import CaseTracking from "@/screens/my-request/complaint/tabs/complaintView/caseTracking";
+import IncomeExpenseMain from "../../screens/treasurer/treasurer-budget-tracker/income-expense-main";
 
 // Makes the Error Overlay dissapear
 import { LogBox } from 'react-native';
@@ -13,6 +14,6 @@ if (__DEV__) {
 }
 
 export default () => {
-    return <App/>
+    return <IncomeExpenseMain/>
 }
  
