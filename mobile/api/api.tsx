95e839e18f0ff97f816e679980d18934ca52073b
Aytsuu <paoloaraneta008@gmail.com>
2025-09-21 12:08:24 +0800

    Update

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -2,23 +2,23 @@ import axios from "axios";
 import { setupApiInterceptor } from "./apiInterceptor";
 import Constants from 'expo-constants'
 
-export const api = axios.create({
-  baseURL: "http://192.168.1.52:8000",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-});
-
 // export const api = axios.create({
-//   baseURL: Constants.expoConfig?.extra?.apiUrl,
+//   baseURL: "http://192.168.1.52:8000",
 //   withCredentials: true,
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Accept": "application/json",
 //   },
 // });
+
+export const api = axios.create({
+  baseURL: Constants.expoConfig?.extra?.apiUrl,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+});
 
 // export const api2 = axios.create({
 //   baseURL: "http://172.31.225.66:8001",
