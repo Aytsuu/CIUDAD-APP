c09231f7571c36214c8b52f6bf8b66f07f93f8e4
lenria <jarlenemaria.garcia@gmail.com>
2025-09-23 19:39:30 +0800

    disbursement mobile start

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -5,6 +5,7 @@ import ResolutionPage from "@/screens/council/resolution/resolution-main";
 import WasteCollectionMain from "@/screens/waste/waste-collection/waste-collection-main";
 import CaseTracking from "@/screens/my-request/complaint/tabs/complaintView/caseTracking";
 import IncomeExpenseMain from "../../screens/treasurer/treasurer-budget-tracker/income-expense-main";
+import DisbursementVoucherList from "@/screens/treasurer/disbursement-voucher/disbursement-main";
 
 // Makes the Error Overlay dissapear
 import { LogBox } from 'react-native';
