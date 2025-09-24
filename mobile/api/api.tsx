519c1ac639b5c9653671c7e31700bd4c0fcdc402
Mariel Amit <marielamit03@gmail.com>
2025-09-24 12:54:14 +0800

    otp

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -2,17 +2,8 @@ import axios from "axios";
 import { setupApiInterceptor } from "./apiInterceptor";
 import Constants from 'expo-constants'
 
-// export const api = axios.create({
-//   baseURL: "http://192.168.1.52:8000",
-//   withCredentials: true,
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json",
-//   },
-// });
-
 export const api = axios.create({
-  baseURL: "http://192.168.179.209:8000", 
+  baseURL: "http://192.168.213.137:8000", 
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -20,7 +11,7 @@ export const api = axios.create({
   },
 });
 export const api2 = axios.create({
-  baseURL: "http://192.168.179.209:8001",
+  baseURL: "http://192.168.213.137:8001",
   timeout: 10000,
 });
 
