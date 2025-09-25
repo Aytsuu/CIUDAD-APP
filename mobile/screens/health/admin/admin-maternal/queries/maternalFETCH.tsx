f4d209ebe0639162bbbab336cffacce39ed0623d
Mariel Amit <marielamit03@gmail.com>
2025-09-26 02:43:09 +0800

    maternal indiv

--- mobile/screens/health/admin/admin-maternal/queries/maternalFETCH.tsx
+++ mobile/screens/health/admin/admin-maternal/queries/maternalFETCH.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getMaternalRecords, getMaternalCount } from "../restful-api/maternalGET";
+import { getMaternalRecords, getMaternalCount, getPregnancyDetails } from "../restful-api/maternalGET";
 
 import { MaternalPatientFilters } from "../restful-api/maternalGET";
 
@@ -15,11 +15,24 @@ export const useMaternalRecords = (filters: MaternalPatientFilters, options = {}
 	});
 }
 
+// for getMaternalCount
 export const useMaternalCount = () => {
 	return useQuery({
 		queryKey: ["activePregnanciesCount"],
 		queryFn: getMaternalCount,
 		staleTime: 60 * 1, 
+		retry: 2,
+	})
+}
+
+// for getPregnancyDetails
+export const usePregnancyDetails = (patientId: string, filters: Partial<MaternalPatientFilters> = {}) => {
+	return useQuery({
+		queryKey: ["pregnancyDetails", { patientId, ...filters }],
+		queryFn: () => getPregnancyDetails({ patientId, ...filters }),
+		enabled: !!patientId,
+		staleTime: 30 * 1,
+		refetchInterval: 2000,
 		retry: 2,
 	})
 }
