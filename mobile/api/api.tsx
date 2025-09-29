7c0a772d7e99da738b9d374784fd3a5766095e96
Mariel Amit <marielamit03@gmail.com>
2025-09-29 10:22:01 +0800

    api change

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -3,7 +3,7 @@ import { setupApiInterceptor } from "./apiInterceptor";
 import Constants from 'expo-constants'
 
 export const api = axios.create({
-  baseURL: "http://192.168.1.4:8000", 
+  baseURL: "http://192.168.1.8:8000", 
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -11,7 +11,7 @@ export const api = axios.create({
   },
 });
 export const api2 = axios.create({
-  baseURL: "http://192.168.1.4:8001",
+  baseURL: "http://192.168.1.8:8001",
   timeout: 10000,
 });
 
