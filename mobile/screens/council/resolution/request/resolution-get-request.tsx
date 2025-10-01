80c7dda1650f2c6215b0cb10089ce74f89e3782b
Baneney <luizsemwarain@gmail.com>
2025-10-01 20:21:08 +0800

    applied backend searching and all in resolution (mobile)

--- mobile/screens/council/resolution/request/resolution-get-request.tsx
+++ mobile/screens/council/resolution/request/resolution-get-request.tsx
@@ -1,14 +1,18 @@
 import {api} from "@/api/api";
 
 
-export const getResolution = async () => {
+export const getResolution = async (searchQuery?: string, areaFilter?: string, yearFilter?: string) => {
     try {
-
-        const res = await api.get('council/resolution/');
-        return res.data;
+        const params: any = {};
+        if (searchQuery) params.search = searchQuery;
+        if (areaFilter && areaFilter !== "all") params.area = areaFilter;
+        if (yearFilter && yearFilter !== "all") params.year = yearFilter;
         
+        const res = await api.get('council/resolution/', { params });
+        return res.data;
     } catch (err) {
         console.error(err);
+        throw err;
     }
 };
 
