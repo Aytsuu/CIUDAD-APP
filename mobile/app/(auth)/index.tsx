b7c8b15e43db26edb323d3d220454c59d60291ba
Hannah Sheen <hannahsheen12@gmail.com>
2025-10-07 00:25:51 +0800

    merge from master

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -17,10 +17,6 @@ import GarbagePickupMain from "@/screens/waste/waste-garbage-pickup-request/staf
 
 // Makes the Error Overlay dissapear
 import { LogBox } from 'react-native';
-import App from "@/screens/auth/login";
-import Family from '../(account)/family';
-import Announcement from '../(announcement)';
-import AnnouncementCreate from '@/screens/announcement/announcementcreate';
 
 if (__DEV__) {
   LogBox.ignoreAllLogs(true);
