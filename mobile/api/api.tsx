c08e8d6b25753423d54cd7e6f69677da9dbe77bf
Hannah Sheen <hannahsheen12@gmail.com>
2025-10-06 13:00:45 +0800

    merge from account v4

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -2,15 +2,6 @@ import axios from "axios";
 import { setupApiInterceptor } from "./apiInterceptor";
 import Constants from 'expo-constants'
 
-export const api = axios.create({
-  baseURL: "http://192.168.1.4:8000",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-});
-
 
 // export const api = axios.create({
 //   baseURL: Constants.expoConfig?.extra?.apiUrl,
