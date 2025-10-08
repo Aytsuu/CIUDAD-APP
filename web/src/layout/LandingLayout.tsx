2614be529d4992ad2652a6c92c7e7539c2e3349f
Aytsuu <paoloaraneta008@gmail.com>
2025-10-09 06:32:22 +0800

    Update

--- web/src/layout/LandingLayout.tsx
+++ web/src/layout/LandingLayout.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button/button";
 import DialogLayout from "@/components/ui/dialog/dialog-layout";
 import SignIn from "@/pages/landing/signin/signin";
 import CebuCitySeal from "@/assets/images/cebucity_seal.svg";
+import SanRoqueLogo from "@/assets/images/sanRoqueLogo.svg";
 import { BadgeCheck } from "lucide-react";
 
 const NavItemList = [
@@ -26,7 +27,8 @@ export default function LandingLayout() {
         <header className="w-full bg-darkBlue1 flex justify-center px-4 py-3">
           <div className="w-[90%] text-white/80 flex items-center gap-5">
             <Label className="font-poppins border-r-2 pr-4 flex items-center gap-1">
-               <BadgeCheck size={16} className="fill-green-600" /> The Official Website of Barangay San Roque (Ciudad)
+              <BadgeCheck size={16} className="fill-green-600" /> The Official
+              Website of Barangay San Roque (Ciudad)
             </Label>
             <div className="flex gap-3">
               <FaFacebook className="hover:text-white cursor-pointer" />
@@ -35,15 +37,30 @@ export default function LandingLayout() {
           </div>
           <img
             src={CebuCitySeal}
-            alt="San Roque Logo"
+            alt="Cebu city official seal"
             className="w-[30px] h-[30px]"
           />
         </header>
         {/* Main Header */}
         <header className="w-full bg-white flex justify-center shadow-lg">
-          <div className="w-full h-full flex justify-center items-center py-4">
+          <div className="w-full h-full flex justify-between items-center">
+            <div className="w-1/3 h-full flex items-center slope-right p-3 bg-[#1273B8]">
+              <div className="w-full flex justify-center items-center gap-3">
+                <img
+                  src={SanRoqueLogo}
+                  alt="San Roque Logo"
+                  className="w-[50px] h-[50px]"
+                />
+                <div className="grid text-white">
+                  <Label className="text-[15px]">
+                    BARANGAY SAN ROQUE (CIUDAD)
+                  </Label>
+                  <Label className="font-poppins">Cebu City</Label>
+                </div>
+              </div>
+            </div>
             {/* Navigation */}
-            <nav className="flex items-center gap-3">
+            <nav className="w-1/2 flex items-center gap-3">
               {NavItemList.map(({ path, title }) => (
                 <Link key={path} to={path}>
                   <Label className="p-[10px] rounded-lg hover:bg-lightBlue cursor-pointer">
@@ -54,22 +71,18 @@ export default function LandingLayout() {
               {user?.staff?.staff_id ? (
                 <Link to={"/dashboard"}>
                   <Button>Dashboard</Button>
-                </Link> 
+                </Link>
               ) : (
-              <DialogLayout
-                trigger={<Button>Sign in</Button>}
-                className="p-0 m-0 border-0 bg-transparent shadow-none outline-none ring-0 focus:ring-0 focus:outline-none focus:border-0 focus:shadow-none max-w-none w-auto h-auto"
-                mainContent={
-                  <SignIn/>
-                }
-              />
+                <DialogLayout
+                  trigger={<Button>Sign in</Button>}
+                  className="p-0 m-0 border-0 bg-transparent shadow-none outline-none ring-0 focus:ring-0 focus:outline-none focus:border-0 focus:shadow-none max-w-none w-auto h-auto"
+                  mainContent={<SignIn />}
+                />
               )}
             </nav>
           </div>
         </header>
-        <header>
-
-        </header>
+        <header></header>
       </div>
 
       {/* Spacer to push content below fixed headers */}
