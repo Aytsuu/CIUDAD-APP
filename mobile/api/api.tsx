a98063908c2523bdcf6058b99b2880f59771f505
betaTrident <colinakb24@gmail.com>
2025-10-15 15:10:22 +0800

    update

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -4,7 +4,7 @@ import Constants from 'expo-constants'
 
 //brgy-api
 export const api = axios.create({
-  baseURL: "http://192.168.1.21:8000",
+  baseURL: "http://192.168.31.86:8000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -14,7 +14,7 @@ export const api = axios.create({
 
 //health-api
 export const api2 = axios.create({
-  baseURL: "http://192.168.1.21:8001",
+  baseURL: "http://192.168.31.86:8001",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
