907a37d6b7e432b6bc8c7aab8e571f088f02422f
Aytsuu <paoloaraneta008@gmail.com>
2025-10-20 23:57:17 +0800

    Update

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -1,36 +1,38 @@
 import axios from "axios";
 import { setupApiInterceptor } from "./apiInterceptor";
+import Constants from 'expo-constants'
 
-//brgy-api
-export const api = axios.create({
-  baseURL: "http://192.168.1.21:8000",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-});
-
-//health-api
-export const api2 = axios.create({
-  baseURL: "http://192.168.1.21:8001",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-});
-
-
-
-// export const api2 = axios.create({
-//   baseURL: Constants.expoConfig?.extra?.apiUrl2,
+// export const api = axios.create({
+//   baseURL: "http://192.168.1.52:8000",
 //   withCredentials: true,
 //   headers: {
-//     "Content-Type": "application/json",r
+//     "Content-Type": "application/json",
 //     "Accept": "application/json",
 //   },
 // });
+
+export const api = axios.create({
+  baseURL: Constants.expoConfig?.extra?.apiUrl,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+});
+
+// export const api2 = axios.create({
+//   baseURL: "http://172.31.225.66:8001",
+//   timeout: 10000,
+// });
+
+export const api2 = axios.create({
+  baseURL: Constants.expoConfig?.extra?.apiUrl2,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+});
 
 setupApiInterceptor(api)
 
