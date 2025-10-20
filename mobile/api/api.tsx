f613aefe8d3a26f0b4322b9f2c2cf13868942369
Baneney <luizsemwarain@gmail.com>
2025-10-20 15:58:54 +0800

    update api mobile

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -19,14 +19,6 @@ export const api = axios.create({
 //   },
 // });
 
-export const api = axios.create({
-  baseURL: "http://10.229.181.53:8000",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-});
 
 // export const api2 = axios.create({
 //   baseURL: Constants.expoConfig?.extra?.apiUrl2,
