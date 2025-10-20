257ceff35f1b37b8f41c9b6fef98a4c44955811a
Chris Tabs <157513312+chanixx786@users.noreply.github.com>
2025-10-20 16:28:42 +0800

    Notification: Added reminder

--- server-1/apps/notification/apps.py
+++ server-1/apps/notification/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 class NotificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.notification'
+
+    def ready(self):
+        from apps.notification.tasks import start_scheduler
+        start_scheduler()
