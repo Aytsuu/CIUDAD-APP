d4903c6ea513ecb20884c4141ac7af64648d16a4
Baneney <luizsemwarain@gmail.com>
2025-10-22 09:35:24 +0800

    clean up waste-main-sched-page

--- web/src/pages/record/waste-scheduling/waste-main-sched-page.tsx
+++ web/src/pages/record/waste-scheduling/waste-main-sched-page.tsx
@@ -1,7 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import EventCalendar from "@/components/ui/calendar/EventCalendar";
 // import WasteHotspotMain from "./waste-hotspot/waste-hotspot-main";
-import WasteCollectionMain from "./waste-collection/waste-col-main";
+// import WasteCollectionMain from "./waste-collection/waste-col-main";
 // import { useGetHotspotRecords } from "./waste-hotspot/queries/hotspotFetchQueries";
 // import { useGetWasteCollectionSchedFull } from "./waste-collection/queries/wasteColFetchQueries";
 import { useState } from "react";
