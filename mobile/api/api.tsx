dbe206d8eb126f8130825f1c757a3095bb5194b2
Kattyshin <caballeskatrina@gmail.com>
2025-11-16 07:12:22 +0800

    .

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -24,19 +24,19 @@ export const api = axios.create({
   },
 });
 
-// export const api2 = axios.create({
-//   baseURL: "http://172.31.225.66:8001",
-//   timeout: 10000,
-// });
-
 export const api2 = axios.create({
-  baseURL: Constants.expoConfig?.extra?.apiUrl2,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
+  baseURL: "http://192.168.1.6:8001",
+  timeout: 10000,
 });
+
+// export const api2 = axios.create({
+//   baseURL: Constants.expoConfig?.extra?.apiUrl2,
+//   withCredentials: true,
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+// });
 
 setupApiInterceptor(api)
 
