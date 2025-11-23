413956fe71ffebe0b8e5b8490b92ec66012b93a3
Aytsuu <paoloaraneta008@gmail.com>
2025-11-23 23:46:00 +0800

    Update

--- server-1/backend/settings.py
+++ server-1/backend/settings.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apps'))
 SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-fallback-key-for-dev-only')
 
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG=True
+DEBUG=config('DEBUG', False)
 
 # ========================
 # SUPABASE CONFIGURATION
