7d07e20e7b46670a5d16217b38e4ae245ba41a8c
Aytsuu <paoloaraneta008@gmail.com>
2025-11-24 19:46:22 +0800

    Update

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -8,7 +8,6 @@ export const mapApi = axios.create({
 
 // export const api = axios.create({
 //   baseURL: "http://192.168.1.52:8000",
-//   withCredentials: true,
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Accept": "application/json",
@@ -18,7 +17,6 @@ export const mapApi = axios.create({
 
 export const api = axios.create({
   baseURL: Constants.expoConfig?.extra?.apiUrl,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
@@ -27,7 +25,6 @@ export const api = axios.create({
 
 // export const api2 = axios.create({
 //   baseURL: "http://172.31.225.66:8001",
-    // withCredentials: true,
     // headers: {
     //   "Content-Type": "application/json",
     //   "Accept": "application/json",
@@ -37,7 +34,6 @@ export const api = axios.create({
 
 export const api2 = axios.create({
   baseURL: Constants.expoConfig?.extra?.apiUrl2,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
--- web/src/api/api.tsx
+++ web/src/api/api.tsx
@@ -7,7 +7,6 @@ export const mapApi = axios.create({
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
@@ -16,7 +15,6 @@ export const api = axios.create({
 
 // export const api = axios.create({
 //   baseURL: "http://localhost:8000",
-//   withCredentials: true, 
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Accept": "application/json",
@@ -25,7 +23,6 @@ export const api = axios.create({
 
 export const api2 = axios.create({
   baseURL: import.meta.env.VITE_API_URL2,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
@@ -34,7 +31,6 @@ export const api2 = axios.create({
 
 // export const api2 = axios.create({
 //   baseURL: "http://localhost:8001",
-//   withCredentials: true, 
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Accept": "application/json",
