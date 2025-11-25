577772782caf3e574c1bdf3c86dc93d0bf0fc148
Aytsuu <paoloaraneta008@gmail.com>
2025-11-25 20:27:49 +0800

    Update

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -6,14 +6,6 @@ export const mapApi = axios.create({
   baseURL: Constants.expoConfig?.extra?.apiUrl,
 })
 
-export const api = axios.create({
-  baseURL: "http://192.168.1.44:8000",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-});
 
 // export const api = axios.create({
 //   baseURL: "http://192.168.1.52:8000",
@@ -24,13 +16,13 @@ export const api = axios.create({
 //   timeout: 10000,
 // });
 
-// export const api = axios.create({
-//   baseURL: Constants.expoConfig?.extra?.apiUrl,
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json",
-//   },
-// });
+export const api = axios.create({
+  baseURL: Constants.expoConfig?.extra?.apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+});
 
 // export const api2 = axios.create({
 //   baseURL: "http://172.31.225.66:8001",
