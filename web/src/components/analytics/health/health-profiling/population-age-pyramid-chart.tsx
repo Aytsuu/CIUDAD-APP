ecc14dbd4fd974ed10ad0cd4a3af5a17620670f6
betaTrident <colinakb24@gmail.com>
2025-11-26 03:29:07 +0800

    pull rec issue

--- web/src/components/analytics/health/health-profiling/population-age-pyramid-chart.tsx
+++ web/src/components/analytics/health/health-profiling/population-age-pyramid-chart.tsx
@@ -7,7 +7,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-import { usePopulationStructureReport } from "../../../../pages/healthServices/Reports/healthprofiling-report/queries/fetchQueries";
+import { usePopulationStructureReport } from "@/pages/healthServices/Reports/healthprofiling-report/queries/fetchQueries";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { AlertCircle, Users, Loader2 } from "lucide-react";
 import { CardTitle } from "@/components/ui/card";
