88d2e2545e6a74eafb9ba8b4f040f5b8edc5f12c
lenria <jarlenemaria.garcia@gmail.com>
2025-11-26 10:49:46 +0800

    minor changes

--- mobile/api/api.tsx
+++ mobile/api/api.tsx
@@ -17,7 +17,7 @@ export const mapApi = axios.create({
 // });
 
 export const api = axios.create({
-  baseURL: "http://10.249.197.53:8000",
+  baseURL: "http://172.16.85.215:8000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
