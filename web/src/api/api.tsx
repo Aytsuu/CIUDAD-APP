ca405677d6ad6ae1cea1910de94139af4b95f733
Aytsuu <paoloaraneta008@gmail.com>
2025-12-08 05:20:42 +0800

    Update

--- web/src/api/api.tsx
+++ web/src/api/api.tsx
@@ -5,25 +5,14 @@ export const mapApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-
-// export const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   withCredentials: true, 
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Accept": "application/json",
-//   },
-// });
-
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
   },
 });
-
 
 // export const api = axios.create({
 //   baseURL: "http://localhost:8000",
@@ -43,11 +32,9 @@ export const api2 = axios.create({
   },
 });
 
-
-
 // export const api2 = axios.create({
 //   baseURL: "http://localhost:8001",
-//   withCredentials: true,
+//   withCredentials: true, 
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Accept": "application/json",
