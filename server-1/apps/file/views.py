d3a264af2541f8c4cf94c68293df2b425a246745
Aytsuu <paoloaraneta008@gmail.com>
2025-12-14 01:58:48 +0800

    Test pushing migrations directory

--- server-1/apps/file/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .serializers.base import *
-from .models import *
-
-class FileView(generics.ListCreateAPIView):
-  serializer_class = FileSerializer
-  queryset = File.objects.all()
-
