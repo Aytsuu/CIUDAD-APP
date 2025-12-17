0ca1e989b41e1e9c1dbda25006b0999f0a4c8957
joshuaganzo <ganzoganzo188@gmail.com>
2025-12-17 13:41:39 +0800

    update

--- web/src/api/api.tsx
+++ web/src/api/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setupApiInterceptor } from "./interceptor";
 
 
-export const mapApi = axios.create({
+export const mapApi = axios.create({ 
   baseURL: import.meta.env.VITE_API_URL,
 });
 
