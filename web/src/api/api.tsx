5ba4fb49865fd226bcb1595d13e8c551be3485d5
joshuaganzo <ganzoganzo188@gmail.com>
2025-12-17 19:25:36 +0800

    update

--- web/src/api/api.tsx
+++ web/src/api/api.tsx
@@ -6,7 +6,7 @@ export const mapApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-export const api = axios.create({
+export const api = axios.create({ 
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, 
   headers: {
