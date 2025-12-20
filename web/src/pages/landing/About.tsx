9403402d9f4db9b62743881e0607c9895d7a28a6
Aytsuu <paoloaraneta008@gmail.com>
2025-12-20 20:11:24 +0800

    Initial commit

--- web/src/pages/landing/About.tsx
+++ web/src/pages/landing/About.tsx
@@ -20,7 +20,7 @@ export default function About({data} : {
   return (
     <div className="relative w-full h-full bg-white overflow-hidden">
       {/* Background Map */}
-      <img
+      <img 
         src={BackgroundPHMap}
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] lg:w-[35%] opacity-10 pointer-events-none"
         alt="map"
